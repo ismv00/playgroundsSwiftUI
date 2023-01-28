55b4fbd2613d21d4f87c838fa3e3eb6a6a79770d
ismv00 <ismv00@icloud.com>
2023-01-28 12:14:41 -0400

    DesafioLoopWithBreak

---
+++ DesafioLoopWithBreak.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let daysOfWeek = ["Segunda", "terça", "quarta", "quinta", "sexta", "sabado", "domingo"]
+
+for i in 0..<daysOfWeek.count {
+    if daysOfWeek[i] == "terça" {
+        continue
+    }
+    if daysOfWeek[i] == "sabado b"{
+        break
+    }
+    print(daysOfWeek[i])
+}
+
+
+
+
